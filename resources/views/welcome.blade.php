c87fff96910ed41e077ac0c56d6de5b667c5cb4b
MangoDowner <353988117@qq.com>
2017-11-02 14:23:39 +0800

    为了防止和laravel的blade模版冲突，vue的变量在{{加上@

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,5 +8,27 @@
   <div id="app"></div>
 
   <script src="{{ asset('js/app.js') }}"></script>
+
+	<div id="app-4">
+	  <ol>
+	    <li v-for="todo in todos">
+	      @{{ todo.text }}
+	    </li>
+	  </ol>
+	</div>
+	<script>
+	var app4 = new Vue({
+	  el: '#app-4',
+	  data: {
+	    todos: [
+	      { text: '学习 JavaScript' },
+	      { text: '学习 Vue' },
+	      { text: '整个牛项目' }
+	    ]
+	  }
+	})
+	</script>
+
+
 </body>
 </html>
